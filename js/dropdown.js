77003646401a40f80b0f7f5ec1008ac1d7aad826
wrafftplort <wrafftplort@gmail.com.vn>
2022-05-31 14:08:57 +0700

    Pooch Care

---
+++ js/dropdown.js
@@ -0,0 +1,16 @@
+function dropDown() {
+    document.getElementById("navbarDropdown1").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown')) {
+        var dropdowns = document.getElementsByClassName("dropdown-menu");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
